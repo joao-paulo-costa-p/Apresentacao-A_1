03046b03dc4ce71dbe822bef24d400f7622bf868
João Paulo <joaopaulo10099@gmail.com>
2025-11-26 01:46:35 -0300

    Add functionality to record student data in a file
    The program now prompts for and records a student's name, grade, and RGM in a file.

--- Algoritmos e Pensamento Computacional/Cadastro simples com gravacao em arquivo/main.c
+++ Algoritmos e Pensamento Computacional/Cadastro simples com gravacao em arquivo/main.c
@@ -1,1 +1,16 @@
+#include <stdio.h>
 
+int main() {
+    FILE *f = fopen("notas.txt", "w"); // Abre o arquivo "alunos.txt" para escrita
+    char nome[30];
+    int nota, RGM;
+    
+    printf("Digite o nome, nota e RGM: "); // Solicita entrada ao usuário
+    scanf("%s %d", nome, &nota, &RGM); //Lê o nome e a idade
+    
+    //Escreve os dados formatados no arquivo
+    fprintf(f, "Nome: %s - idade: %d - RGM: %d\n", nome, nota, RGM);
+    fclose(f); //Fecha o arquivo
+    
+    return 0;
+}
